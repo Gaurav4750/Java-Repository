40a3ade4e40dc9e1583a2cf86dc50c7b5a9de318
Gaurav Mukherjee <89590945+Gaurav4750@users.noreply.github.com>
2023-12-02 01:14:26 +0530

    Create StringMagicRunner.java

---
+++ String/StringMagicRunner.java
@@ -0,0 +1,13 @@
+import StringMagic.*;
+import java.lang.*;
+import java.util.*;
+
+public class StringMagicRunner{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		
+		StringMagic str = new StringMagic();
+		str = 
+	}
+}
